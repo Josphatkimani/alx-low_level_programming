233ea8650cef422ad0147d02a57fce581ddec401
lawsons254 <josphatkimani02@gmail.com>
2023-06-16 01:59:06 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with put function
+ * Return:0
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

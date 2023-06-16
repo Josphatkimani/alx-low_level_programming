3dd41e72bfc264e5690908b2c2b0e6eedab41635
lawsons254 <josphatkimani02@gmail.com>
2023-06-16 12:43:37 -0700

    Changes

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Starting point
+* Return: 0
+*/
+int main(void)
+{
+	int number;
+
+	for
+		(number = 0;
+		number <= 9;
+		number++) {
+			putchar(number + '0');
+	}
+	putchar('\n');
+	return (0);
+}

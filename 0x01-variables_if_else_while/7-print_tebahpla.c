7fe98c9177bd70567c5e4822ddfcc49a8ba9bdd6
lawsons254 <josphatkimani02@gmail.com>
2023-06-16 13:17:53 -0700

    changes

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Starting point
+* Return: 0
+*/
+int main(void)
+{
+	char letter;
+
+	for
+		(letter = 'z';
+		letter >= 'a';
+		letter--) {
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}

e128ec36086c04b8f9a5f4297f3119d6bc139ee3
lawsons254 <josphatkimani02@gmail.com>
2023-06-16 03:08:43 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line to the standard
+ * Return: 1
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}

ccda980ccffd78f8311f9201316d3183c24cf826
lawsons254 <josphatkimani02@gmail.com>
2023-06-16 13:05:42 -0700

    Changes

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Starting point
+* Return: 0
+*/
+int main(void)
+{
+	int number;
+
+	for
+		(number = 0;
+		number <= 9;
+		number++) {
+			putchar(number + 48);
+	}
+	putchar('\n');
+	return (0);
+}

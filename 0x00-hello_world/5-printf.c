57f106c694315c47869cc98f80621c76f1217e49
lawsons254 <josphatkimani02@gmail.com>
2023-06-16 02:07:07 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the prinf function
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

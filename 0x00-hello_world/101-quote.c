a5aefb5d72e345398296fddcee7318ee8a456931
lawsons254 <josphatkimani02@gmail.com>
2023-06-19 09:00:29 -0700

    Changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints a line to the standard
+ * main - A program that prints a line
  * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }

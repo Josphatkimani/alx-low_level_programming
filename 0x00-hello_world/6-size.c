dde0837af078e3dfe206ea63a6eacfec9aa08afe
lawsons254 <josphatkimani02@gmail.com>
2023-06-19 08:44:39 -0700

    New changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ long int c;
 long long int d;
 float e;
 printf("Size of a char: %lu byte(s)\n", sizeof(a));
-printf("Size of a char: %lu byte(s)\n", sizeof(b));
-printf("Size of a char: %lu byte(s)\n", sizeof(c));
-printf("Size of a char: %lu byte(s)\n", sizeof(d));
-printf("Size of a char: %lu byte(s)\n", sizeof(e));
+printf("Size of an int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+printf("Size of a float: %lu byte(s)\n", sizeof(e));
 return (0);
 }
